d839b402abe1fc085a986238b3da95d1aa94ca7d
luis <lrlitang18@gmail.com>
2023-06-25 03:06:34 -0500

    feat: add change room and remove device

--- lib/components/custom-app-bar.dart
+++ lib/components/custom-app-bar.dart
@@ -2,26 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:light_management/screens/lights-list.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-
   @override
   Widget build(BuildContext context) {
-    return  AppBar(
-        actions: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
+    return AppBar(
+      actions: [
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const LightsList()),
             );
-            },
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Icon(Icons.home),
-            ),
+          },
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Icon(Icons.home),
           ),
-        ],
-        title: const Text('Lights Management'),
-      );
+        ),
+      ],
+      title: const Text('Lights Management'),
+    );
   }
 
   @override
